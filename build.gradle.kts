8635550df467d2b361661b45d14c25c4468767f6
klaasschuijtemaker <klaas.schuijtemaker@ritense.com>
2025-05-22 16:49:08 +0200

    refactor

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,6 @@ java.targetCompatibility = JavaVersion.VERSION_21
 repositories {
     mavenCentral()
     maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
-    maven { url = uri("https://valtimo-snapshots.s3.eu-central-1.amazonaws.com/") }
     maven { url = uri("https://repo.ritense.com/repository/maven-public/") }
     maven { url = uri("https://repo.ritense.com/repository/maven-snapshot/") }
 }
--- src/main/kotlin/com/ritense/valtimo/Application.kt
+++ src/main/kotlin/com/ritense/valtimo/Application.kt
@@ -32,10 +32,13 @@ fun main(args: Array<String>) {
 
     logger.info {
         """
+
+        ----------------------------------------------------------
         Application '${environment.getProperty("spring.application.name")}' is running!
         Active profile(s): [${environment.getProperty("spring.profiles.active")}].
         Local URL: [http://127.0.0.1:${environment.getProperty("server.port")}].
         External URL: [http://${InetAddress.getLocalHost().hostAddress}:${environment.getProperty("server.port")}]
+        ----------------------------------------------------------
         """.trimIndent()
     }
 }
