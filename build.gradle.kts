40b18976b9ad7af468c722e12a45fa757b49e025
Thomas Minke <thomas.minke@ritense.com>
2025-07-03 20:17:09 +0200

    Upgrade to 13.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,9 +29,6 @@ java.targetCompatibility = JavaVersion.VERSION_21
 
 repositories {
     mavenCentral()
-    maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
-    maven { url = uri("https://repo.ritense.com/repository/maven-public/") }
-    maven { url = uri("https://repo.ritense.com/repository/maven-snapshot/") }
 }
 
 val valtimoVersion: String by project
