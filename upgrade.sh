a49a1fdc981bf3ce6711cf4aca320e4831063788
Klaas Schuijtemaker <klaas.schuijtemaker@ritense.com>
2026-06-16 10:29:48 +0200

    update freemarker plugin to version 8.3.6

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git fetch && git checkout v/${VALTIMO_RELEASE_MAJOR_VERSION} && git pull
-sed -i '' '/valtimoVersion/d' ./gradle.properties && echo "valtimoVersion=${VALTIMO_RELEASE_VERSION}" >> ./gradle.properties
+sed -i.bak '/valtimoVersion/d' ./gradle.properties && rm -f ./gradle.properties.bak && echo "valtimoVersion=${VALTIMO_RELEASE_VERSION}" >> ./gradle.properties
 git add gradle.properties && git commit -m "Upgrade to ${VALTIMO_RELEASE_VERSION}" && git push
 
